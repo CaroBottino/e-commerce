20174de3b679f741ee0b80a9451a2fedddaf4e39
Carolina Bottino <carolinabottino@MBP-de-Carolina.fibertel.com.ar>
2023-08-13 23:56:01 -0300

    agrego definicion de interfaz a items

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { useEffect, useState } from "react";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import ItemListContainer from "./components/ItemListContainer";
+import { IItem } from "./interfaces/IItem";
 import "@/assets/css/App.css";
 
 import { items as data } from "./assets/js/items";
 
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState<IItem[]>([]);
 
   useEffect(() => {
-    setItems(data);
+    setItems(data as IItem[]);
   }, []);
 
   return (
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="vite/client" />
+declare module './assets/js/items'
