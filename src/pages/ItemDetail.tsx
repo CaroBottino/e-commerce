d7aadf2119523a55403d88c723519656c70035f5
Carolina Bottino <carolina.bottino@devbase.us>
2024-04-09 17:23:55 -0300

    fix: item detail

--- src/pages/ItemDetail.tsx
+++ src/pages/ItemDetail.tsx
@@ -1,28 +1,11 @@
-import { useParams } from "react-router-dom";
-import ItemDetailContainer from "../components/CWItemDetailContainer";
 import "@/assets/css/ItemDetailPage.css";
-import { useEffect } from "react";
+import ItemDetailContainer from "../components/CWItemDetailContainer";
 import ItemsProvider from "../providers/Items.provider";
-import CWItemDetailSkeleton from "../components/CWSkeletons/CWItemDetailSkeleton";
-import { useItemsContext } from "../hooks/useItemsContext";
 
 const ItemDetailPage = () => {
-  const { id } = useParams();
-  const { loading, getItemInfo, item } = useItemsContext();
-
-  useEffect(() => {
-    id && getItemInfo(id);
-  }, [id]);
-
   return (
     <ItemsProvider>
-      {loading ? (
-        <CWItemDetailSkeleton />
-      ) : item ? (
-        <ItemDetailContainer item={item} />
-      ) : (
-        <div>Item not found</div>
-      )}
+      <ItemDetailContainer />
     </ItemsProvider>
   );
 };
